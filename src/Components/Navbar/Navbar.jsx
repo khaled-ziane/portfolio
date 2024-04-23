229f3b8198a759d87f42d8519be4bcb9eca293ac
samikoum <khaledinfo2015@gmail.com>
2024-04-23 12:11:00 +0100

    add portolio

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faBars } from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  // Menu functionality
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  // Add the icons
+
+  library.add(faCircleXmark, faBars);
+
+  return (
+    <div className="navbar">
+      <nav>
+        <h1>Khaled Ziane</h1>
+        <ul className={menuOpen ? "active" : ""}>
+          <li>
+            <a href="#header">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <FontAwesomeIcon icon={faCircleXmark} onClick={toggleMenu} />
+        </ul>
+        <FontAwesomeIcon icon={faBars} onClick={toggleMenu} />
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
